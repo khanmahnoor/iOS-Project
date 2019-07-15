30c1ad0f07bc7027320c4fb514ca996d8103bc35
khanmahnoor <mahnoorkhan@tintash.com>
2019-07-15 18:20:03 +0500

    day 21

---
+++ Football News/Football News/Model/NewsFeedObject.swift
@@ -0,0 +1,17 @@
+//
+//  NewsFeedObject.swift
+//  Football News
+//
+//  Created by Mahnoor Khan on 15/07/2019.
+//  Copyright © 2019 Mahnoor Khan. All rights reserved.
+//
+
+import Foundation
+
+struct NewsFeedObject : Codable {
+    var url : String
+    var title : String
+    var description : String
+    var type : Int
+    var key : Int
+}
