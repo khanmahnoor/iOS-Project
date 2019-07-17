5840fc0b4d624d8c7146d54347f56763efb0c7f3
khanmahnoor <mahnoorkhan@tintash.com>
2019-07-17 12:47:17 +0500

    Adding swift and nib file for prototype video cell

---
+++ Football News/Football News/Network Layer/NetworkEngine.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkLayer.swift
+//  Football News
+//
+//  Created by Mahnoor Khan on 17/07/2019.
+//  Copyright © 2019 Mahnoor Khan. All rights reserved.
+//
+
+import Foundation
+
+protocol NetworkEngine {
+    
+}
--- Football News/Football News/NewsFeedTab/View Controller/FNVideoCell.swift
+++ Football News/Football News/NewsFeedTab/View Controller/FNVideoCell.swift
@@ -1,41 +1,33 @@
 //
-//  FNVideoCell.swift
+//  VideoCell.swift
 //  Football News
 //
-//  Created by Mahnoor Khan on 16/07/2019.
+//  Created by Mahnoor Khan on 17/07/2019.
 //  Copyright © 2019 Mahnoor Khan. All rights reserved.
 //
 
 import UIKit
 import youtube_ios_player_helper
-
 class FNVideoCell: UITableViewCell {
 
-    
-    @IBOutlet weak var videoPlayer: YTPlayerView!
+    @IBOutlet weak var video: YTPlayerView!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
-    func lala (_ u : String) {
-        videoPlayer.delegate = self
-        videoPlayer.load(withVideoId: u)
-//        videoPlayer.loadVideo(byURL: u, startSeconds: 0.0, suggestedQuality: .auto)
+    func loadVideo(_ u : String) {
+        video.delegate = self
+        video.load(withVideoId: u)
+        //videoPlayer.loadVideo(byURL: u, startSeconds: 0.0, suggestedQuality: .auto)
     }
-
 }
 
 extension FNVideoCell : YTPlayerViewDelegate {
     func playerViewDidBecomeReady(_ playerView: YTPlayerView) {
-//        videoPlayer.playVideo()
-
+        //videoPlayer.playVideo()
     }
-    
 }
